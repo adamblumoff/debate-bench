69a3dc79d92638948aba249fe663a52ae8a4b9b9
adamblumoff <ablumoff25@amherst.edu>
2025-12-01 17:15:41 -0600

    Fix VegaLite dynamic import crash by using direct client import

--- dashboard/src/components/VegaLiteChart.tsx
+++ dashboard/src/components/VegaLiteChart.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import { VisualizationSpec } from "vega-embed";
-
-const VegaLite = dynamic(() => import("react-vega").then((m) => m.VegaLite), { ssr: false });
+import { VegaLite, VisualizationSpec } from "react-vega";
 
 export function VegaLiteChart({ spec }: { spec: VisualizationSpec }) {
   return <VegaLite spec={spec} actions={false} className="w-full" />;
