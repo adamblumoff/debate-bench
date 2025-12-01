ba4353b0820c182f8868126776675f58431c869f
adamblumoff <ablumoff25@amherst.edu>
2025-12-01 17:17:23 -0600

    Pass embed options for Vega to avoid invalid actions prop warning

--- dashboard/src/components/VegaLiteChart.tsx
+++ dashboard/src/components/VegaLiteChart.tsx
@@ -3,5 +3,5 @@
 import { VegaEmbed, VisualizationSpec } from "react-vega";
 
 export function VegaLiteChart({ spec }: { spec: VisualizationSpec }) {
-  return <VegaEmbed spec={spec} actions={false} className="w-full" />;
+  return <VegaEmbed spec={spec} options={{ actions: false }} className="w-full" />;
 }
